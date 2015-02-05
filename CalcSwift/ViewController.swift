9d2d8db82df2423ea23fbd3117db9a8dabeb89e1
“KEEVIN <keevin.mitchell@gmail.com>
2015-02-05 18:20:10 -0500

    Interface Design

--- CalcSwift/ViewController.swift
+++ CalcSwift/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel! //This is set when we hook up to storyboard so we just unwrap it. implicitly unwrapped optional.
+    
+    var userIsInTheMiddleOfTypingNumber:Bool = false
+   
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        let digit = sender.currentTitle! // didnt put type for variable. nil means not set. ! unwraps the optional if its set and crashes if its nil.
+        println("digit = \(digit)")
+        
+        //Now that we have the digit let add it (append it) to the end of the display label.
+        if userIsInTheMiddleOfTypingNumber{
+        
+        display.text = display.text! + digit
+        } else{
+           //Get rid of zero when typing number
+          display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
+        
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
