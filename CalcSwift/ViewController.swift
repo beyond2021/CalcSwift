9483930a3b8d7970a69462c7899ce7277ab1935d
“KEEVIN <keevin.mitchell@gmail.com>
2015-02-05 20:19:11 -0500

    Add Multiply case

--- CalcSwift/ViewController.swift
+++ CalcSwift/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel! //This is set when we hook up to storyboard so we just unwrap it. implicitly unwrapped optional.
     
-    var userIsInTheMiddleOfTypingNumber:Bool = false
-   
+   // var userIsInTheMiddleOfTypingNumber:Bool = false
+    var userIsInTheMiddleOfTypingNumber = false //inferred
+    
     @IBAction func appendDigit(sender: UIButton) {
         
         let digit = sender.currentTitle! // didnt put type for variable. nil means not set. ! unwraps the optional if its set and crashes if its nil.
@@ -30,8 +31,71 @@ class ViewController: UIViewController {
         }
         
         
+    }
+    
+    //Our Stack
+   // var operandStack:Array<Double> = Array<Double>() //Initializing with an empty array with no arguments. Every property MUST be initialized.
+    var operandStack = Array<Double>()//inferred.
+    
+    @IBAction func operate(sender: UIButton) {
+       
+        
+        //We will do the same thing we will get the senders current title
+        let operation = sender.currentTitle!
+        
+        //Lets give the user an automatic enter.
+        if userIsInTheMiddleOfTypingNumber{
+            enter()
+            
+        }
+        
+        
+        
+    //We are going to switch on the operation string
+        switch operation{
+            //multiply the last 2 things on the stack
+            case "×":
+                if operandStack.count >= 2{
+            displayValue = operandStack.removeLast() * operandStack.removeLast()
+                    enter() //Automatic enter here too
+            }
+            
+//            command / to comment stuff out
+//            case "÷":
+//            case "+":
+//            case "−":
+        default: break //here we break out of the switch
+        }
         
         
     }
+    
+    
+    @IBAction func enter() {
+      //We want to put the numbers in our display on to our stack
+        userIsInTheMiddleOfTypingNumber = false
+        operandStack.append(displayValue)
+        println("Operand Stack = \(operandStack)") //OperanStack is an array
+        
+        
+    }
+    //Computed property
+    // we want to use display.text but we need a double and its a string. So we have to (compute )calculate it.
+    var displayValue: Double
+        {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            
+        }
+        set{
+            
+        // convert newVale into text "\(newValue)"
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingNumber = false // someone set the display text
+        }
+        
+        
+    }
+    
 }
 
